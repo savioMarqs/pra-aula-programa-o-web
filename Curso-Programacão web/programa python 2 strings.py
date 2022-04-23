f37b7d46235b15817b5510819f386cd1897e1b57
savioMarqs <102438441+savioMarqs@users.noreply.github.com>
2022-04-23 10:56:23 -0300

    Add files via upload

---
+++ Curso-Programacão web/programa python 2 strings.py
@@ -0,0 +1,10 @@
+#Desenvolva um programa Python que devolva leia um nome e uma data e retorne uma String. Neste caso o programa vai 
+# falar a data de nascimento de alguém.
+nome = input("Digite o Nome: ").title()
+dia = input("Digite o dia: ").title()
+mes = input("Digite o mês: ").title()
+ano = input("Digite o ano: ").title()
+
+
+txt = "{} nasceu no dia: {}/{}/{}"
+print(txt.format(nome,dia,mes,ano))
