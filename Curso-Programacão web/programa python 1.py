977806787a6487b0af76d5220c97375391592c2b
savioMarqs <102438441+savioMarqs@users.noreply.github.com>
2022-04-22 22:52:38 -0300

    Add files via upload

---
+++ Curso-Programacão web/programa python 1.py
@@ -0,0 +1,12 @@
+#Desenvolva um programa em Python que leia o primeiro nome de uma pessoa e seu 
+# salário do mês de abril e na sequência mostre uma mensagem formatada.
+
+nome = input("Insira seu Nome: ")
+salário = input("Insira o valor do seu salário")
+
+NOMEdapessoa = nome
+Osalario = salário
+
+Otexto = "o salário de " + NOMEdapessoa + " no mês de abril foi de: " + salário
+
+print(Otexto)
