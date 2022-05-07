b18a0b8a2d573d6285d46b3acd5ad6bb4fec796b
savioMarqs <102438441+savioMarqs@users.noreply.github.com>
2022-05-06 22:38:16 -0300

    Add files via upload

---
+++ Curso-Programacão web/programa python 3.py
@@ -0,0 +1,6 @@
+largura = input ("insira a largura ")
+comprimento = input("insira o comprimento ")
+areaDoLugar = float(largura) * float(comprimento)
+areaDoLugar / 2
+Texto = 'serão nescessarios ' + str(areaDoLugar) + 'litros' + 'de' + 'produto para a realização da limpeza.'
+print(Texto)
